f7e99594d1048e7e0f1b40f982b054e8d55b3fcd
FadwaElouah <fadwaelouah@gmail.com>
2024-12-19 23:59:35 +0100

    ajouter la formulaire pour la page inscription

--- inscription.php
+++ inscription.php
@@ -21,7 +21,37 @@
          <a href="connexion.php">Connexion</a>
        </div>
 </header>
- 
+ <section>
+    <div class="form-box">
+        <h2>Inscription</h2>
+        <form action="register.php" method="POST">
+            <div class="form-group">
+                <label for="username">Nom d'utilisateur</label>
+                <input type="text" id="username" name="username" required>
+            </div>
+
+            <div class="form-group">
+                <label for="email">Email</label>
+                <input type="email" id="email" name="email" required>
+            </div>
+
+            <div class="form-group">
+                <label for="password">Mot de passe</label>
+                <input type="password" id="password" name="password" required>
+            </div>
+
+             <div class="form-group">
+                <label for="role">Rôle</label>
+                <select name="role" id="role" required>
+                    <option value="client">Client</option>
+                    <option value="avocat">Avocat</option>
+                </select>
+                </div>
+
+                <button type="submit">S'inscrire</button>
+        </form>
+      </div>
+ </section>
 
 <script src="app.js"></script>
 </body>
